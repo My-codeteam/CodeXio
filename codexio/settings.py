71e77319fe28ba0a61ab18eba7bd0570bf0e3190
MarvCode <marvcode.co@gmail.com>
2025-04-20 15:06:28 -0500

    updated settings.py

--- codexio/settings.py
+++ codexio/settings.py
@@ -135,4 +135,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
