9ec5fc4a67e5b8e3d0ada90cd277cf4a16f644de
MarvCode <marvcode.co@gmail.com>
2025-05-27 15:40:32 -0500

    updated the index.css file

--- codexio_main/views.py
+++ codexio_main/views.py
@@ -123,10 +123,10 @@ def home(request):
     return render(request, 'codexio_main/index.html')
 
 def custom_404(request, exception):
-    return render(request, '404.html', status=404)
+    return render(request, 'codexio_main/404.html', status=404)
 
 def custom_500(request):
-    return render(request, '500.html', status=500)
+    return render(request, 'codexio_main/500.html', status=500)
 
 
 
