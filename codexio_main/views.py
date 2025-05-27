934a19344dc105a787142810f6fe0e44ea8fc38c
Destiny Efe Omorhienrhien <81220100+destiny620@users.noreply.github.com>
2025-05-27 13:33:40 +0100

    Update views.py

--- codexio_main/views.py
+++ codexio_main/views.py
@@ -122,6 +122,11 @@ chatbot_pairs = [
 def home(request):
     return render(request, 'codexio_main/index.html')
 
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
 
 
 
