1a187b567ab3164572da105dffabed15381615c2
PeterOlayemi <olayemipeter2005@gmail.com>
2025-10-14 15:40:53 +0100

    configured environment variables and requirements

--- codexio/settings.py
+++ codexio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q_x0ejrlh+$b0m_ve$_klp&!l&bj@dm+i2lom6vn3a6xe6i+ya'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
