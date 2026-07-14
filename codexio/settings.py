60323d3a98f1014eca07fda9dee55d44c0aac19f
MarvCode <marvcode.co@gmail.com>
2026-07-14 14:50:01 +0000

    revived previous changes to the settings file

--- codexio/settings.py
+++ codexio/settings.py
@@ -163,22 +163,19 @@ load_dotenv()
 FLW_PUBLIC_KEY = os.getenv("FLW_PUBLIC_KEY")
 FLW_SECRET_KEY = os.getenv("FLW_SECRET_KEY")
 
-EMAIL_BACKEND = config(
-    "EMAIL_BACKEND",
-    default="django.core.mail.backends.console.EmailBackend",
-)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = config("EMAIL_HOST", default="")
+EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_PORT = 587
 
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER or "noreply@codexio.test")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 GITHUB_TOKEN = config("GITHUB_TOKEN")
 
